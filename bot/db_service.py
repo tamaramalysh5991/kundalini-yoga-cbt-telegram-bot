86e037703114e2776be66882605cf86a04612066
TamaraMalyheva <tamaramalysheva5991@gmail.com>
2024-02-07 22:01:42 +0100

    fix mongo db connection
    * add docker file

--- bot/db_service.py
+++ bot/db_service.py
@@ -1,6 +1,10 @@
 from pymongo import MongoClient
+from .config import Config
 
-
-client = MongoClient("mongodb://mongodb:27017/")
+client = MongoClient(
+    Config.MONGODB_URI,
+    username=Config.MONGO_INITDB_ROOT_USERNAME,
+    password=Config.MONGO_INITDB_ROOT_PASSWORD,
+)
 db = client["telegram_bot_db"]
 user_collection = db["users"]
