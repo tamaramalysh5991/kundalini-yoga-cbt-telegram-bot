411e9078f62d6d9846d7dea56772f7e92f7ceb91
TamaraMalyheva <tamaramalysheva5991@gmail.com>
2024-02-25 22:05:33 +0100

    add basic reminder each 2 minutes

--- bot/main.py
+++ bot/main.py
@@ -1,16 +1,37 @@
 from aiogram import Bot, Dispatcher
 
+from db_service import users_collection
 from config import Config
 from aiogram.fsm.storage.memory import MemoryStorage
 from handlers import router
 import asyncio
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
+bot = Bot(token=Config.TELEGRAM_TOKEN)
+
+
+scheduler = AsyncIOScheduler()
+
+
+async def send_reminder():
+    users = users_collection.find()
+    message_content = "Привет! Делай йогу!"
+    for user in users:
+        try:
+            await bot.send_message(user["chat_id"], message_content)
+        except Exception as e:
+            print(f"Failed to send message to {user}: {e}")
 
 
 async def main():
     # Bot and Dispatcher setup
-    bot = Bot(token=Config.TELEGRAM_TOKEN)
     dp = Dispatcher(storage=MemoryStorage())
     dp.include_router(router)
+
+    # Schedule the reminder task
+    scheduler.add_job(send_reminder, "interval", minutes=2, id="my job 1")
+    scheduler.start()
+
     await dp.start_polling(bot)
 
 
